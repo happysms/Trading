16446c51fccf4eda81b613da8781be761bd00162
MinSeongSim <70648382+happysms@users.noreply.github.com>
2021-07-04 22:32:40 +0900

    Update bitcoinAutoTrade.py

--- bitcoinAutoTrade.py
+++ bitcoinAutoTrade.py
@@ -1,9 +1,13 @@
 import time
 import pyupbit
 import datetime
+import requests
+from slacker import Slacker
 
-access = "d"
-secret = "d"
+slack = Slacker('')
+
+access = ""
+secret = ""
 
 def get_target_price(ticker, k):
     """변동성 돌파 전략으로 매수 목표가 조회"""
@@ -17,9 +21,16 @@ def get_start_time(ticker):
     start_time = df.index[0]
     return start_time
 
+def get_ma15(ticker):
+    """15일 이동 평균선 조회"""
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=15)
+    ma15 = df['close'].rolling(15).mean().iloc[-1]
+    return ma15
+
 def get_balance(ticker):
     """잔고 조회"""
     balances = upbit.get_balances()
+    print(balances)
     for b in balances:
         if b['currency'] == ticker:
             if b['balance'] is not None:
@@ -30,13 +41,16 @@ def get_balance(ticker):
 
 def get_current_price(ticker):
     """현재가 조회"""
+    print("현재가 조회 ")
     return pyupbit.get_orderbook(tickers=ticker)[0]["orderbook_units"][0]["ask_price"]
 
 # 로그인
 upbit = pyupbit.Upbit(access, secret)
 print("autotrade start")
+# 시작 메세지 슬랙 전송
+slack.chat.post_message("#stock", "autotrade start")
+print("슬랙 전송")
 
-# 자동매매 시작
 while True:
     try:
         now = datetime.datetime.now()
@@ -45,18 +59,22 @@ while True:
 
         if start_time < now < end_time - datetime.timedelta(seconds=10):
             target_price = get_target_price("KRW-BTC", 0.5)
+            ma15 = get_ma15("KRW-BTC")
             current_price = get_current_price("KRW-BTC")
-            if target_price < current_price:
+            if target_price < current_price and ma15 < current_price:
+                
                 krw = get_balance("KRW")
                 if krw > 5000:
-                    upbit.buy_market_order("KRW-BTC", krw*0.9995)
+                    buy_result = upbit.buy_market_order("KRW-BTC", krw*0.05)
+                    slack.chat.post_message("#stock", "BTC buy : " +str(buy_result))
         else:
             btc = get_balance("BTC")
+            
             if btc > 0.00008:
-                upbit.sell_market_order("KRW-BTC", btc*0.9995)
+                sell_result = upbit.sell_market_order("KRW-BTC", btc*0.05)
+                slack.chat.post_message("#stock", "BTC buy : " +str(sell_result))
         time.sleep(1)
     except Exception as e:
         print(e)
+        slack.chat.post_message("#stock", e)
         time.sleep(1)
-
-        
