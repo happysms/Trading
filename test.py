099f54c3712f3447fa2f4a35b550321426c1b221
MinSeongSim <70648382+happysms@users.noreply.github.com>
2021-08-18 13:34:04 +0900

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import pyupbit
-
-access = "d"          # 본인 값으로 변경
-secret = "d"          # 본인 값으로 변경
-upbit = pyupbit.Upbit(access, secret)
-
-print(upbit.get_balance("KRW-XRP"))     # KRW-XRP 조회
-print(upbit.get_balance("KRW"))         # 보유 현금 조회
