7ce410336e7673d8e24d9220899f0e00910bb45a
minsung <khuminsung@khu.ac.kr>
2021-09-30 00:40:51 +0900

    바이낸스 선물 투자 봇 구현

---
+++ future_volatility/future_trading_bot.py
@@ -0,0 +1,69 @@
+import ccxt
+import time
+import datetime
+import util
+import argparse
+
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('--api_key', metavar='api_key', required=True,
+                    help='api_key')
+parser.add_argument('--secret', metavar='secret', required=True,
+                    help='secret')
+
+args = parser.parse_args()
+api_key = args.api_key
+secret = args.secret
+
+# binance 객체 생성
+binance = ccxt.binance(config={
+    'apiKey': api_key,
+    'secret': secret,
+    'enableRateLimit': True,
+    'options': {
+        'defaultType': 'future'
+    }
+})
+
+op_mode = False
+symbol = "BTC/USDT"
+long_target, short_target = util.cal_target(binance, symbol)
+
+# 잔고 조회
+balance = binance.fetch_balance()
+usdt = balance['total']['USDT']
+
+position = {
+    "type": None,
+    "amount": 0
+}
+
+print("long 목표가: ", long_target, "\nshort 목표가: ", short_target)
+
+while True:
+    now = datetime.datetime.now()
+
+    # position 종료
+    if now.hour == 8 and now.minute == 50 and (20 <= now.second < 30):
+        if op_mode and position['type'] is not None:
+            util.exit_position(binance, symbol, position)
+            op_mode = False
+
+    # 목표가 갱신
+    if now.hour == 9 and now.minute == 0 and (20 <= now.second < 30):
+        long_target, short_target = util.cal_target(binance, symbol)
+        balance = binance.fetch_balance()
+        usdt = balance['total']['USDT']
+        op_mode = True
+        time.sleep(10)
+
+    # 현재가, 구매 가능 수량
+    btc = binance.fetch_ticker(symbol=symbol)
+    cur_price = btc['last']
+    amount = util.cal_amount(usdt, cur_price, 0.5)
+
+    if op_mode and position['type'] is None:
+        util.enter_position(binance, symbol, cur_price, long_target, short_target, amount, position)
+
+    print("long 목표가: ", long_target, "\nshort 목표가: ", short_target)
+    print(now, "현재가 : ", cur_price)
+    time.sleep(1)
---
+++ future_volatility/util.py
@@ -0,0 +1,52 @@
+import math
+import pandas as pd
+
+
+def cal_target(exchange, symbol):
+    data = exchange.fetch_ohlcv(
+        symbol=symbol,
+        timeframe='1d',
+        since=None,
+        limit=3
+    )
+
+    df = pd.DataFrame(
+        data=data,
+        columns=['datetime', 'open', 'high', 'low', 'close', 'volume']
+    )
+
+    df['datetime'] = pd.to_datetime(df['datetime'], unit='ms')
+    df.set_index('datetime', inplace=True)
+
+    yesterday = df.iloc[-2]
+    today = df.iloc[-1]
+
+    long_target = today['open'] + (yesterday['high'] - yesterday['low']) * 0.5
+    short_target = today['open'] - (yesterday['high'] - yesterday['low']) * 0.5
+
+    return long_target, short_target
+
+
+def cal_amount(usdt_balance, cur_price, portion):
+    usdt_trade = usdt_balance * portion
+    amount = math.floor((usdt_trade * 1000000) / cur_price) / 1000000
+    return amount
+
+
+def enter_position(exchange, symbol, cur_price, long_target, short_target, amount, position):
+    if cur_price > long_target:
+        position['type'] = 'long'
+        position['amount'] = amount
+        exchange.create_market_buy_order(symbol=symbol, amount=amount)
+    elif cur_price < short_target:
+        position['type'] = 'short'
+        position['amount'] = amount
+        exchange.create_market_sell_order(symbol=symbol, amount=amount)
+
+
+def exit_position(exchange, symbol, position):
+    amount = position['amount']
+    if position['type'] == 'long':
+        exchange.create_market_sell_order(symbol=symbol, amount=amount)
+    elif position['type'] == 'short':
+        exchange.create_market_buy_order(symbol=symbol, amount=amount)
