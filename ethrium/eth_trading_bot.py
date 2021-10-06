78eef89d38943909d548e907705b7b51b8bf8ed4
minsung <khuminsung@khu.ac.kr>
2021-10-06 22:11:06 +0900

    eth 전날 10% 상승 제한 추가 및 매도타임 증가

--- ethrium/eth_trading_bot.py
+++ ethrium/eth_trading_bot.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime, timedelta
-from pytz import timezone
 import time
 import pyupbit
 import datetime
@@ -29,7 +28,9 @@ def get_target_price(ticker, k):
     """변동성 돌파 전략으로 매수가 조회"""
     df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
     target_price = df.iloc[0]['close'] + (df.iloc[0]['low']) * k
-    return target_price
+    before_day_condition = (df.iloc[0]['close'] / df.iloc[0]['open']) < 1.1
+
+    return target_price, before_day_condition
 
 
 if __name__ == "__main__":
@@ -46,16 +47,15 @@ if __name__ == "__main__":
     secret = args.secret
     token = args.token
 
-    KST = timezone('Asia/Seoul')
     upbit = pyupbit.Upbit(access, secret)
     mylogger = logger.make_logger("main")
-
     mylogger.info("ETH Trading Start")
+    op_mode = False
 
     while True:
         try:
-            now = (KST.localize(datetime.datetime.now()))
-            start_time = KST.localize(datetime.datetime(now.year, now.month, now.day, 9, 1, 0))
+            now = datetime.datetime.now()
+            start_time = datetime.datetime(now.year, now.month, now.day, 9, 1, 0)
 
             if now.hour < 9:
                 start_time = start_time - timedelta(days=1)
@@ -63,10 +63,10 @@ if __name__ == "__main__":
             end_time = start_time + timedelta(days=1)
 
             if start_time < now < end_time - timedelta(seconds=10):  # 가격 감시 시간
-                target_price = get_target_price(ticker="KRW-ETH", k=0.4)
+                target_price, condition = get_target_price(ticker="KRW-ETH", k=0.4)
                 current_price = get_current_price("KRW-ETH")
 
-                if target_price <= current_price:
+                if target_price <= current_price and op_mode and condition:
                     krw = upbit.get_balance("KRW")
 
                     if krw > 5006:  # 최소 주문 금액
@@ -77,6 +77,8 @@ if __name__ == "__main__":
                 eth = get_balance("ETH")
                 sell_result = upbit.sell_market_order("KRW-ETH", eth)
                 mylogger.info(f"ETH {eth}개 매도 주문 ")
+                op_mode = True
+                time.sleep(10)
 
             time.sleep(0.5)
 
