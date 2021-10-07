0e11e14638b553d591f55f9c302abc3791e35c9f
minsung <khuminsung@khu.ac.kr>
2021-10-08 00:11:09 +0900

    이더리움 선물 트레이딩 봇 ma20 & before day condition 코드 추가

--- future_volatility/future_trading_bot.py
+++ future_volatility/future_trading_bot.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import ccxt
 import time
 import datetime
@@ -24,20 +25,28 @@ binance = ccxt.binance(config={
     }
 })
 
-op_mode = False
-symbol = "BTC/USDT"
-long_target, short_target = util.cal_target(binance, symbol)
+
+symbol = "ETH/USDT"
+long_target, short_target, ma20_condition, before_day_condition = util.cal_target(binance, symbol)
 
 # 잔고 조회
 balance = binance.fetch_balance()
 usdt = balance['total']['USDT']
 
+eth = binance.fetch_ticker(symbol=symbol)
+cur_price = eth['last']
+
 position = {
     "type": None,
     "amount": 0
 }
+op_mode = False
 
 print("long 목표가: ", long_target, "\nshort 목표가: ", short_target)
+
+if short_target <= cur_price <= long_target:
+    print("프로그램 실행 시점에서 거래 가능 구간")
+    op_mode = True
 
 while True:
     now = datetime.datetime.now()
@@ -47,23 +56,25 @@ while True:
         if op_mode and position['type'] is not None:
             util.exit_position(binance, symbol, position)
             op_mode = False
+            position['type'] = None
 
     # 목표가 갱신
     if now.hour == 9 and now.minute == 0 and (20 <= now.second < 30):
-        long_target, short_target = util.cal_target(binance, symbol)
+        long_target, short_target, ma20_condition, before_day_condition = util.cal_target(binance, symbol)
+
         balance = binance.fetch_balance()
         usdt = balance['total']['USDT']
         op_mode = True
         time.sleep(10)
 
     # 현재가, 구매 가능 수량
-    btc = binance.fetch_ticker(symbol=symbol)
-    cur_price = btc['last']
+    eth = binance.fetch_ticker(symbol=symbol)
+    cur_price = eth['last']
     amount = util.cal_amount(usdt, cur_price, 0.95)
 
     if op_mode and position['type'] is None:
-        util.enter_position(binance, symbol, cur_price, long_target, short_target, amount, position)
+        position['type'], invest_amount = util.enter_position(binance, symbol, cur_price, long_target, short_target, amount, position, ma20_condition, before_day_condition)
+        if position['type']:
+            print(f"{position['type']}포지션 진입, 투자금 {invest_amount}")
 
-    print("long 목표가: ", long_target, "\nshort 목표가: ", short_target)
-    print(now, "현재가 : ", cur_price)
     time.sleep(1)
--- future_volatility/util.py
+++ future_volatility/util.py
@@ -7,7 +7,7 @@ def cal_target(exchange, symbol):
         symbol=symbol,
         timeframe='1d',
         since=None,
-        limit=3
+        limit=21
     )
 
     df = pd.DataFrame(
@@ -17,14 +17,20 @@ def cal_target(exchange, symbol):
 
     df['datetime'] = pd.to_datetime(df['datetime'], unit='ms')
     df.set_index('datetime', inplace=True)
-
     yesterday = df.iloc[-2]
     today = df.iloc[-1]
 
-    long_target = today['open'] + (yesterday['high'] - yesterday['low']) * 0.5
-    short_target = today['open'] - (yesterday['high'] - yesterday['low']) * 0.5
+    ma20 = df['close'].rolling(window=20).mean().iloc[-1]
+    is_rise = False
+    before_day_condition = (yesterday['close'] / yesterday['open']) < 1.08
 
-    return long_target, short_target
+    if today['open'] > ma20:
+        is_rise = True
+
+    long_target = today['open'] + (yesterday['high'] - yesterday['low']) * 0.4
+    short_target = today['open'] - (yesterday['high'] - yesterday['low']) * 0.4
+
+    return long_target, short_target, is_rise, before_day_condition
 
 
 def cal_amount(usdt_balance, cur_price, portion):
@@ -33,16 +39,17 @@ def cal_amount(usdt_balance, cur_price, portion):
     return amount
 
 
-def enter_position(exchange, symbol, cur_price, long_target, short_target, amount, position):
-    if cur_price > long_target:
+def enter_position(exchange, symbol, cur_price, long_target, short_target, amount, position, ma20_condition, before_day_condition):
+    if cur_price > long_target and ma20_condition and before_day_condition:
         position['type'] = 'long'
         position['amount'] = amount
         exchange.create_market_buy_order(symbol=symbol, amount=amount)
-    elif cur_price < short_target:
+    elif cur_price < short_target and not ma20_condition:
         position['type'] = 'short'
         position['amount'] = amount
         exchange.create_market_sell_order(symbol=symbol, amount=amount)
 
+    return position['type'], position['amount']
 
 def exit_position(exchange, symbol, position):
     amount = position['amount']
